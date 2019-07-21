87ae4e998554589bded598679cd92ed2f6fe8353
sajafisher29 <sajafisher@gmail.com>
2019-07-21 16:59:53 -0700

    Test 2 passed

--- code-challenges/chaining/challenges-10.test.js
+++ code-challenges/chaining/challenges-10.test.js
@@ -26,7 +26,14 @@ For example, [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]] returns 66.
 ------------------------------------------------------------------------------------------------ */
 
 const totalSum = (input) => {
-  // Solution code here...
+  let rowValues = new Array(input.length).fill(0);
+  for (let i = 0; i <= input.length-1; i++) {
+    for (let j = 0; j <= input[i].length-1; j++) {
+      let value = input[i][j];
+      rowValues[i] += value;
+    }
+  }
+  return rowValues.reduce((a, b) => a + b, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
