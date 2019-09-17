b51508afb12764911b78d0fa79f7f7e0c4c79ac1
Sarah Fisher <sajafisher@gmail.com>
2019-09-16 21:04:46 -0700

    stretch goal completed

--- code401challenges/src/main/java/code401challenges/ArrayReverse.java
+++ code401challenges/src/main/java/code401challenges/ArrayReverse.java
@@ -3,8 +3,9 @@ import java.util.Arrays;
 public class ArrayReverse {
 
   public static void main(String[] args) {
-    int[] arrToReverse = new int[]{2, 6, 9, 12};
+    int[] arrToReverse = new int[]{2, 6, 9, 12, 15};
     int[] reversedArray = reverseArray(arrToReverse);
+    int[] shrunkArray = shrinkArray(arrToReverse);
     System.out.println(Arrays.toString(reversedArray));
   }
 
@@ -19,5 +20,22 @@ public class ArrayReverse {
       count++;
     }
     return reversedArray;
+  }
+
+  public static int[] shrinkArray(int[] arr) {
+
+    int[] shrunkArray = new int[arr.length];
+    
+    int begin = 0;
+    int end = arr.length-1;
+
+    while(begin < end) {
+      int tempHold = arr[begin];
+      arr[end] = arr[begin];
+      arr[begin] = tempHold;
+      begin++;
+      end--;
+    }
+    return shrunkArray;
   }
 }
