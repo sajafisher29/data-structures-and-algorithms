25ab876d269274455e5874ba40ee8fe76fb54a15
Sarah Fisher <sajafisher@gmail.com>
2019-10-09 09:24:56 -0700

    Building tests for FizzBuzz.

--- 401codechallenges/src/test/java/code401challenges/tree/FizzBuzzTreeTest.java
+++ 401codechallenges/src/test/java/code401challenges/tree/FizzBuzzTreeTest.java
@@ -1,8 +1,24 @@
 package code401challenges.tree;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class FizzBuzzTreeTest {
 
+    // test working fizz -> only divisible by 3
+    @Test public void testFizz() {
+
+    }
+
+    // test working buzz -> only divisible by 5
+    @Test public void testBuzz() {
+
+    }
+
+    // test working fizzbuzz -> divisible by three and five
+    @Test public void testFizzBuzz() {
+
+    }
+
+    //What if current is null?
 
 }
